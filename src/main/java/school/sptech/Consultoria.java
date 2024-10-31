1ff6553771f5d2ed4f500e7a24f07891a3162e03
Julia-Apolinario <julia.apolinario@sptech.school>
2024-10-31 13:25:35 -0300

    Atividade Julia-ApolinarioV1

--- src/main/java/school/sptech/Consultoria.java
+++ src/main/java/school/sptech/Consultoria.java
@@ -1,4 +1,108 @@
 package school.sptech;
 
-public class Consultoria {
+
+import school.sptech.especialistas.DesenvolvedorMobile;
+import school.sptech.especialistas.DesenvolvedorWeb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Consultoria extends DesenvolvedorWeb{
+    private String nome;
+    private Integer vagas;
+    private List<Desenvolvedor> desenvolvedores;
+
+    public Consultoria() {
+    }
+
+    public Consultoria(String nome, Integer vagas, List<Desenvolvedor> desenvolvedores) {
+        this.nome = nome;
+        this.vagas = vagas;
+        this.desenvolvedores = desenvolvedores;
+    }
+
+    public Boolean contratar (Desenvolvedor desenvolvedor){
+       if (vagas > desenvolvedores.size() ){
+           desenvolvedores.add(desenvolvedor);
+           return true;
+       }return false;
+    }
+
+    public Boolean contratarFullstack(DesenvolvedorWeb desenvolvedor){
+
+                if (desenvolvedor.isFullstack()){
+                    desenvolvedores.add(desenvolvedor);
+                    return true;
+                }
+        return false;
+    }
+
+    public Double getTotalSalarios(){
+        Double soma = 0.0;
+        for (Desenvolvedor desenvolvedorDaVez : desenvolvedores){
+            soma += desenvolvedorDaVez.calcularSalario();
+
+        }
+        return soma;
+    }
+
+    public Integer qtdDesenvolvedoresMobile() {
+        Integer cont = 0;
+        for (Desenvolvedor desenvolvedorDaVez : desenvolvedores) {
+            if (desenvolvedorDaVez instanceof DesenvolvedorMobile) {
+                cont++;
+            }
+
+        }return cont;
+    }
+
+    public List<Desenvolvedor> buscarPorSalarioMaiorIgualQue(Double salario){
+        List<Desenvolvedor> maiorSalario = new ArrayList<>();
+        for (Desenvolvedor desenvolvedorDaVez : desenvolvedores) {
+            if (desenvolvedorDaVez.calcularSalario() >= salario) {
+
+                maiorSalario.add(desenvolvedorDaVez);
+
+            }
+        }return maiorSalario;
+    }
+
+//    public Desenvolvedor buscarMenorSalario(){
+//        List<Desenvolvedor> maiorSalario = new ArrayList<>();
+//        for (Desenvolvedor desenvolvedorDaVez : desenvolvedores) {
+//            if (desenvolvedorDaVez.calcularSalario() >= salario) {
+//
+//                maiorSalario.add(desenvolvedorDaVez);
+//
+//            }
+//        }return maiorSalario;
+//    }
+
+
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getVagas() {
+        return vagas;
+    }
+
+    public void setVagas(Integer vagas) {
+        this.vagas = vagas;
+    }
+
+    public List<Desenvolvedor> getDesenvolvedores() {
+        return desenvolvedores;
+    }
+
+    public void setDesenvolvedores(List<Desenvolvedor> desenvolvedores) {
+        this.desenvolvedores = desenvolvedores;
+    }
 }
--- src/main/java/school/sptech/especialistas/DesenvolvedorMobile.java
+++ src/main/java/school/sptech/especialistas/DesenvolvedorMobile.java
@@ -1,4 +1,54 @@
 package school.sptech.especialistas;
 
-public class DesenvolvedorMobile {
+import school.sptech.Desenvolvedor;
+
+public class DesenvolvedorMobile extends Desenvolvedor {
+    private String plataforma;
+    private String linguagem;
+    private Integer horasPrototipacao;
+
+    public DesenvolvedorMobile() {
+
+    }
+
+    public DesenvolvedorMobile(String pataforma, String linguagem, Integer horasPrototipacao) {
+        this.plataforma = pataforma;
+        this.linguagem = linguagem;
+        this.horasPrototipacao = horasPrototipacao;
+    }
+
+    public DesenvolvedorMobile(String nome, Integer qtdHora, Double valorHora, String pataforma, String linguagem, Integer horasPrototipacao) {
+        super(nome, qtdHora, valorHora);
+        this.plataforma = pataforma;
+        this.linguagem = linguagem;
+        this.horasPrototipacao = horasPrototipacao;
+    }
+
+    public Double calcularSalario(){
+        return super.calcularSalario() + 200 * horasPrototipacao;
+    }
+
+    public String getPlataforma() {
+        return plataforma;
+    }
+
+    public void setPlataforma(String plataforma) {
+        this.plataforma = plataforma;
+    }
+
+    public String getLinguagem() {
+        return linguagem;
+    }
+
+    public void setLinguagem(String linguagem) {
+        this.linguagem = linguagem;
+    }
+
+    public Integer getHorasPrototipacao() {
+        return horasPrototipacao;
+    }
+
+    public void setHorasPrototipacao(Integer horasPrototipacao) {
+        this.horasPrototipacao = horasPrototipacao;
+    }
 }
